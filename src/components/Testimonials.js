58d3a5c9141d86df7e10ca983d591c826ae4f9c7
Haruta Tasaki <hrtune@gmail.com>
2023-12-28 17:21:37 +0900

    Add template to Testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,39 @@
+const RatingCard = ({ rating, image, name, review }) => {
+  return (
+    <article>
+      <header>
+        <h3>{rating}</h3>
+      </header>
+      <section>
+        <img src={image} alt={name} />
+        <h4>{name}</h4>
+      </section>
+      <section>
+        <p>{review}</p>
+      </section>
+    </article>
+  );
+};
 const Testimonials = () => {
-  return <></>;
+  const ratings = [
+    {
+      rating: 5,
+      image: "",
+      name: "John",
+      review:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.",
+    },
+  ];
+  return (
+    <section>
+      <header>
+        <h2>Testimonials</h2>
+      </header>
+      <section>
+        <RatingCard {...ratings[0]} />
+      </section>
+    </section>
+  );
 };
 
 export default Testimonials;
