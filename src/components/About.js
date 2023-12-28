ab7f5b7d97ff3063bf859daf824922a2fe6e2900
Haruta Tasaki <hrtune@gmail.com>
2023-12-28 18:08:53 +0900

    Add template to About

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,24 @@
 const About = () => {
-  return <></>;
+  return (
+    <section>
+      <section>
+        <header>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+        </header>
+        <section>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit; Aenean
+            commodo ligula eget dolor.
+          </p>
+        </section>
+      </section>
+      <aside>
+        <img src="" alt="Image1" />
+        <img src="" alt="Image2" />
+      </aside>
+    </section>
+  );
 };
 
 export default About;
