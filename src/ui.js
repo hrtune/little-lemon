0759f5a1fb64bcc6d725452584f42f8d2bb0b82c
Haruta Tasaki <hrtune@gmail.com>
2023-12-28 18:44:34 +0900

    Create style guide

---
+++ src/ui.js
@@ -0,0 +1,23 @@
+const ui = {
+  color: {
+    green: "#495E57",
+    yellow: "#F4CE14",
+    white: "#FFFFFF",
+    black: "#000000",
+    pink: "#F3916F",
+    lightGreen: "#EAEDEB",
+  },
+  fontFamily: {
+    header: "Markazi Text Medium",
+    paragraph: "KARLA REGULAR",
+  },
+  fontSize: {
+    title: "64px",
+    subTitle: "40px",
+    leadText: "18px",
+    cardTitle: "18px",
+    paragraph: "16px",
+  },
+};
+
+export default ui;
