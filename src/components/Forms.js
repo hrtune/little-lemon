9401beb86247775e3c8581625f5a9912498df859
Haruta Tasaki <hrtune@gmail.com>
2023-12-29 21:20:08 +0900

    Implement stateful form

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,5 +1,92 @@
+import { useReducer } from "react";
+const initialData = {
+  date: "",
+  time: "17:00",
+  guests: "1",
+  occasion: "Birthday",
+};
+const reducer = (state, action) => {
+  // action: { name: "", value:"" }
+  const newState = { ...state };
+  newState[action.name] = action.value;
+  return newState;
+};
 const Forms = () => {
-  return <></>;
+  const [data, dispatch] = useReducer(reducer, initialData);
+
+  const formStyle = {
+    backgroundColor: "grey",
+    height: "500px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "space-around",
+    padding: "0 290px",
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(data);
+  };
+
+  return (
+    <form style={formStyle} onSubmit={submit}>
+      <section>
+        <label htmlFor="date">Choose date:</label>
+        <input
+          type="date"
+          name="date"
+          id="res-date"
+          value={data.date}
+          onChange={(e) => dispatch({ name: "date", value: e.target.value })}
+        />
+      </section>
+      <section>
+        <label htmlFor="time">Choose time:</label>
+        <select
+          name="time"
+          id="res-time "
+          value={data.time}
+          onChange={(e) => dispatch({ name: "time", value: e.target.value })}
+        >
+          <option>17:00</option>
+          <option>18:00</option>
+          <option>19:00</option>
+          <option>20:00</option>
+          <option>21:00</option>
+          <option>22:00</option>
+        </select>
+      </section>
+      <section>
+        <label htmlFor="guests">Number of guests:</label>
+        <input
+          type="number"
+          name="guests"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="res-guests"
+          value={data.guests}
+          onChange={(e) => dispatch({ name: "guests", value: e.target.value })}
+        />
+      </section>
+      <section>
+        <label htmlFor="occasion">Occasion:</label>
+        <select
+          name="occasion"
+          id="res-occasion"
+          value={data.occasion}
+          onChange={(e) =>
+            dispatch({ name: "occasion", value: e.target.value })
+          }
+        >
+          <option>Birthday</option>
+          <option>Anniversary</option>
+        </select>
+      </section>
+      <button type="submit">Make Your Reservation</button>
+    </form>
+  );
 };
 
 export default Forms;
